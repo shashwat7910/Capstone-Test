7ca430f96ae1498a0298589dd93f336840584cc6
shassingh <shassingh@teksystems.com>
2021-05-20 16:40:07 +0530

    Tests

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CapstoneTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CapstoneTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d6a3832-f455-4048-b770-4a11e32a1550")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace CapstoneTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:4200/");
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(
+                    d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")
+                    );
+                var username = driver.FindElementById("user-field");
+                username.SendKeys("User123");
+                var pass = driver.FindElementById("pass-field");
+                pass.SendKeys("1234");
+                var btn = driver.FindElementById("login-btn");
+                btn.Click();
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(
+                    d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")
+                    );
+                var adminpanel = driver.FindElementById("admin-btn");
+                adminpanel.Click();
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(
+                    d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")
+                    );
+                var access= driver.PageSource.Contains("403");
+                Assert.IsTrue(access);
+                driver.Navigate().Refresh();
+                var logout = driver.FindElementById("logout-btn");
+                logout.Click();
+                
+                driver.Navigate().Refresh();
+                username = driver.FindElementById("user-field");
+                username.SendKeys("123@admin");
+                pass = driver.FindElementById("pass-field");
+                pass.SendKeys("1234");
+                btn = driver.FindElementById("login-btn");
+                btn.Click();
+                driver.Navigate().Refresh();
+                adminpanel = driver.FindElementById("admin-btn");
+                adminpanel.Click();
+                var medicine = driver.PageSource.Contains("403");
+                Assert.IsFalse(medicine);
+                var home = driver.FindElementById("home-btn");
+                home.Click();
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(
+                    d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")
+                    );
+                
+            }
+        }
+    }
+}
+
